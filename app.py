b8042de4c7af4868f5a8b7623b691bdc62469fd0
DylanHalstead <dylanhalstead11@gmail.com>
2022-03-23 15:59:00 -0400

    Fixed up app.py

--- app.py
+++ app.py
@@ -5,3 +5,23 @@ app = Flask(__name__)
 @app.get('/')
 def index():
     return render_template('index.html')
+
+@app.get('/movies')
+def all_movies():
+    return render_template('all_movies.html')
+
+@app.get('/posts')
+def all_posts():
+    return render_template('all_posts.html')
+
+@app.get('/create-post')
+def create_post():
+    return render_template('create_post.html')
+
+@app.get('/edit-account')
+def edit_account():
+    return render_template('edit_account.html')
+
+@app.get('/report')
+def report():
+    return render_template('report.html')
