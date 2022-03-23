1abe8000de5796946d35ea898b7ca760b37912b9
ahadinger <amhadinger@gmail.com>
2022-03-23 13:20:24 -0400

    create class files for future implementation

---
+++ src/user.py
@@ -0,0 +1,8 @@
+class User:
+
+    def __init__(self, username: str, picture: str, points: int) -> None:
+        self.username = username
+        self.picture = picture
+        self.points = points
+    
+    
