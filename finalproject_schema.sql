1a682f9a157cc965f4e84fa55fb826a1d2ee6445
DylanHalstead <dylanhalstead11@gmail.com>
2022-04-04 16:58:39 -0400

    Fixed up database, think it's 100% done?

--- finalproject_schema.sql
+++ finalproject_schema.sql
@@ -1,42 +1,65 @@
--- Re/create DB
-DROP DATABASE IF EXISTS final;
-CREATE DATABASE final;
-USE final;
+DROP DATABASE IF EXISTS cinemaholics_db;
+CREATE DATABASE cinemaholics_db;
+USE cinemaholics_db;
 
-
-CREATE TABLE users (
-    user_id INT AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL,
-    user_email VARCHAR(255) NOT NULL,
-    user_password VARCHAR(255) NOT NULL,
-    user_pfp VARCHAR(255) NOT NULL,
-    user_about TEXT(65535),
+CREATE TABLE IF NOT EXISTS users (
+    users_id   INT           AUTO_INCREMENT,
+    username  VARCHAR(255)  NOT NULL,
+    email     VARCHAR(255)  NOT NULL,
+    passkey   VARCHAR(255)  NOT NULL,
+    pfp       VARCHAR(255)  NOT NULL,
+    about     TEXT(65535)   NULL,
     PRIMARY KEY (user_id)    
 );
 
-CREATE TABLE posts (
-    post_id INT AUTO_INCREMENT,
-    user_id INT,
-    title VARCHAR(255) NOT NULL,
-    body    TEXT(65535) NOT NULL,
-    post_time VARCHAR(255) NOT NULL,
-    likes   INT NOT NULL,
+CREATE TABLE IF NOT EXISTS posts (
+    post_id    INT           AUTO_INCREMENT,
+    users_id   INT           NOT NULL,
+    title      VARCHAR(255)  NOT NULL,
+    body       TEXT(65535)   NOT NULL,
+    post_time  VARCHAR(255)  NOT NULL,
+    likes      INT           NOT NULL,
     PRIMARY KEY (post_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-CREATE TABLE replies (
-	reply_id INT AUTO_INCREMENT,
-    post_id INT,
-    user_id INT,
-    body TEXT(65535) NOT NULL,
-    post_time VARCHAR(255) NOT NULL,
-    likes INT NOT NULL,
+CREATE TABLE IF NOT EXISTS replies (
+	reply_id   INT            AUTO_INCREMENT,
+    post_id    INT            NOT NULL,
+    users_id   INT            NOT NULL,
+    body       TEXT(65535)    NOT NULL,
+    post_time  VARCHAR(255)   NOT NULL,
+    likes      INT            NOT NULL,
     PRIMARY KEY (reply_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (post_id) REFERENCES posts(post_id)
 );
 
+CREATE TABLE IF NOT EXISTS users_playlist  (
+	users_id     INT,
+	playlist_id  INT,
+    PRIMARY KEY (users_id, playlist_id),
+    FOREIGN KEY (users_id) REFERENCES users(users_id),
+    FOREIGN KEY (playlist_id) REFERENCES playlist(playlist_id)
+);
 
+CREATE TABLE IF NOT EXISTS playlist  (
+	playlist_id     INT             AUTO_INCREMENT,
+    playlist_name   VARCHAR(255),
+    PRIMARY KEY (playlist_id)
+);
 
+CREATE TABLE IF NOT EXISTS playlist_movie (
+    playlist_id    INT,
+    movie_id       INT,
+    PRIMARY KEY (playlist_id, movie_id),
+    FOREIGN KEY (playlist_id) REFERENCES playlist(playlist_id),
+    FOREIGN KEY (movie_id) REFERENCES movie(movie_id)
+);
 
+CREATE TABLE IF NOT EXISTS movie  (
+	-- poster_URL, title, IMDB_rating, genre, etc... will all come from the movie_id (used to find IMDB page)
+	movie_id     INT,
+    UNCC_rating  DECIMAL,
+    PRIMARY KEY (movie_id)
+);
--- src/post.py
+++ src/post.py
@@ -1,8 +1,7 @@
 class Post:
 
-    def __init__(self, user, movie, text, points, replies, time) -> None:
+    def __init__(self, user, text, points, replies, time) -> None:
         self.user = user #user object that created reply
-        self.movie = movie #movie that post is about
         self.text = text #text of the reply
         self.points = points #updoots and whatnot
         self.replies = replies #list of all replies to the post
