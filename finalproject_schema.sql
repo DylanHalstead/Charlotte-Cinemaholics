e3644adc7af76d0645775b5977a78dfbd158ea8d
ahadinger <amhadinger@gmail.com>
2022-04-06 19:34:17 -0400

    fix typos and other errors with the sql file

--- finalproject_schema.sql
+++ finalproject_schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE cinemaholics_db;
 USE cinemaholics_db;
 
 CREATE TABLE IF NOT EXISTS users (
-    users_id   INT          AUTO_INCREMENT,
+    user_id   INT          AUTO_INCREMENT,
     username  VARCHAR(255)  NOT NULL,
     email     VARCHAR(255)  NOT NULL,
     passkey   VARCHAR(255)  NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS posts (
     post_id    INT           AUTO_INCREMENT,
-    users_id   INT           NOT NULL,
+    user_id   INT           NOT NULL,
     title      VARCHAR(255)  NOT NULL,
     body       TEXT(65535)   NOT NULL,
     post_time  VARCHAR(255)  NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS posts (
 CREATE TABLE IF NOT EXISTS replies (
 	reply_id   INT            AUTO_INCREMENT,
     post_id    INT            NOT NULL,
-    users_id   INT            NOT NULL,
+    user_id   INT            NOT NULL,
     body       TEXT(65535)    NOT NULL,
     post_time  VARCHAR(255)   NOT NULL,
     likes      INT            NOT NULL,
@@ -35,18 +35,25 @@ CREATE TABLE IF NOT EXISTS replies (
     FOREIGN KEY (post_id) REFERENCES posts(post_id)
 );
 
+
+CREATE TABLE IF NOT EXISTS playlist  (
+	playlist_id     INT		AUTO_INCREMENT,
+    playlist_name   VARCHAR(255) NOT NULL,
+    PRIMARY KEY (playlist_id)
+);
+
 CREATE TABLE IF NOT EXISTS users_playlist  (
-	users_id     INT,
+	user_id     INT,
 	playlist_id  INT,
-    PRIMARY KEY (users_id, playlist_id),
-    FOREIGN KEY (users_id) REFERENCES users(users_id),
+    PRIMARY KEY (user_id, playlist_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (playlist_id) REFERENCES playlist(playlist_id)
 );
 
-CREATE TABLE IF NOT EXISTS playlist  (
-	playlist_id     INT             AUTO_INCREMENT,
-    playlist_name   VARCHAR(255),
-    PRIMARY KEY (playlist_id)
+CREATE TABLE IF NOT EXISTS movie  (
+	movie_id     INT,
+    UNCC_rating  DECIMAL,
+    PRIMARY KEY (movie_id)
 );
 
 CREATE TABLE IF NOT EXISTS playlist_movie (
@@ -57,9 +64,3 @@ CREATE TABLE IF NOT EXISTS playlist_movie (
     FOREIGN KEY (movie_id) REFERENCES movie(movie_id)
 );
 
-CREATE TABLE IF NOT EXISTS movie  (
-	-- poster_URL, title, IMDB_rating, genre, etc... will all come from the movie_id (used to find IMDB page)
-	movie_id     INT,
-    UNCC_rating  DECIMAL,
-    PRIMARY KEY (movie_id)
-);
