c3189ec3d22e8c48b16f0e0baffa2317f36f5640
ahadinger <amhadinger@gmail.com>
2022-04-08 20:29:00 -0400

    add initial post and reply functionality

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
 from blueprints.posts_blueprint import router as posts_router
 from blueprints.movie_blueprint import router as movie_router
 from blueprints.account_blueprint import router as account_router
+from src.models import db
 
-import os
-from dotenv import load_dotenv
+
 load_dotenv()
 db_user = os.getenv('DB_USER', 'root')
-db_pass = os.getenv('DB_PASS')
+db_pass = os.getenv('DB_PASS', '12345')
 db_host = os.getenv('DB_HOST', 'localhost')
 db_port = os.getenv('DB_PORT', '3306')
-db_name = os.getenv('DB_NAME', 'final')
+db_name = os.getenv('DB_NAME', 'cinemaholics_db')
 connection_string = f'mysql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = connection_string
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+db.init_app(app)
 
 @app.get('/')
 def index():
