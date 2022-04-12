5324e29a7321c48ddacecae4d99b6ab57a4033be
ahadinger <amhadinger@gmail.com>
2022-04-11 23:10:38 -0400

    make posts use actual users and allow for editing and deleting of posts

--- src/blueprints/posts_blueprint.py
+++ src/blueprints/posts_blueprint.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, redirect, render_template, request
+from flask import Blueprint, abort, redirect, render_template, request, session
 from datetime import datetime
 from src.models import Post, Reply, User, db
 import random #need to remove
@@ -19,38 +19,22 @@ def get_post(post_id):
     users = User.query.all()
     user = User.query.filter_by(user_id=post.user_id).first()
     return render_template('post.html', post = post, replies = replies, user = user, users = users)
-    
-@router.post('/<post_id>/')
-def create_reply(post_id):
-    createDummyUsers()
-    #user = TODO get current user
-    user_id = random.randrange(1,4) #TODO change to actual user
-    body = request.form.get('body', '')
-    now = datetime.now()
-    time = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-    likes = 0
-
-    if body == '':
-        abort(400)
-
-    reply = Reply(post_id = post_id, user_id=user_id, body=body, post_time = time, likes = likes)
-    db.session.add(reply)
-    db.session.commit()
-
-    return redirect(f'/posts/{post_id}')
 
 
 @router.get('/new')
 def get_new_post_form():
-    return render_template('create_post.html')
+    if 'user' in session:
+        return render_template('create_post.html')
+    else: 
+        return redirect('/posts/')
 
 @router.post('/')
 def create_post():
-    createDummyUsers()
     title = request.form.get('title', '')
     print(title)
     #user = TODO get current user
-    user_id = random.randrange(1,4) #TODO change to actual user
+
+    user_id = session['user'].get('user_id')
     body = request.form.get('body', '')
     print(body)
     time = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
@@ -66,15 +50,70 @@ def create_post():
 
     return redirect(f'/posts/{new_post.post_id}')
 
+@router.post('/<post_id>/')
+def create_reply(post_id):
+    #createDummyUsers()
+    if 'user' in session:
+        user_id = session['user'].get('user_id')
+        body = request.form.get('body', '')
+        now = datetime.now()
+        time = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        likes = 0
 
+        if body == '':
+            abort(400)
 
+        reply = Reply(post_id = post_id, user_id=user_id, body=body, post_time = time, likes = likes)
+        db.session.add(reply)
+        db.session.commit()
+
+    return redirect(f'/posts/{post_id}')
 
 #TODO add edit post page
+@router.get('/<post_id>/edit')
+def get_edit_book_form(post_id): 
+    #TODO add check if user is a moderator as well
+    post_to_edit = Post.query.get_or_404(post_id)
+    if 'user' in session:
+        user_id = session['user'].get('user_id')
+        if post_to_edit.user_id == user_id:
+            return render_template('edit_post.html', post=post_to_edit)
+            
+    return redirect(f'/posts/{post_id}')
+
+
+
+@router.post('/<post_id>/edit')
+def update_book(post_id): 
+    post = Post.query.get_or_404(post_id)
+    title = request.form.get('title', '')
+    body = request.form.get('body', '')
+
+    if body == '' or title == '':
+        abort(400)
+
+    post.title = title
+    post.body = body
+
+    db.session.commit()
+
+    return redirect(f'/posts/{post_id}')
+
 
 #TODO add delete post page
+@router.post('/<post_id>/delete')
+def delete_post(post_id):
+    post_to_delete = Post.query.get_or_404(post_id)
+    if 'user' in session:
+        user_id = session['user'].get('user_id')
+        if post_to_delete.user_id == user_id:
+            db.session.delete(post_to_delete)
+            db.session.commit()
+            return redirect('/posts/')
+    return redirect('/posts/{post_id}')
 
 
-def createDummyUsers(): #dummy users to test post functionality, when logging in gets implemented this will be removed
+def createDummyUsers(): #dummy users to test post functionality
     if User.query.first() == None:
         user = User(username = "unccstudent", email = "aa", passkey = "sjs", pfp = "pfp1.png", about = "")
         user2 = User(username = "austin", email = "p", passkey = "sds", pfp = "pfp5.png", about = "")
