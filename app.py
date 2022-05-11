14fc861cae0b23a2f57deca4784ff43cf123ef80
DylanHalstead <dylanhalstead11@gmail.com>
2022-05-11 18:51:51 -0400

    fixed commit clearing issue

--- app.py
+++ app.py
@@ -13,12 +13,6 @@ from flask_sqlalchemy import SQLAlchemy
 load_dotenv()
 
 app = Flask(__name__)
-db_user = os.getenv('DB_USER', 'root')
-db_pass = os.getenv('DB_PASS', '12345')
-db_host = os.getenv('DB_HOST', 'localhost')
-db_port = os.getenv('DB_PORT', '3306')
-db_name = os.getenv('DB_NAME', 'cinemaholics_db')
-connection_string = f'mysql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('CLEARDB_DATABASE_URL', 'sqlite:///test.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -31,7 +25,6 @@ db.init_app(app)
 @app.get('/')
 def index():
     # Grab first 5 top films
-    session.clear()
     for movie in range(5):
         if not isinstance(top_films[movie], dict):
             pass
@@ -73,13 +66,11 @@ def favicon():
 def report():
     return render_template('report.html')
 
-@app.get('/username')
-def account():
-    return render_template('account.html')
-
 @app.get('/username/edit')
 def edit_account():
-    return render_template("edit_account.html")
+    if 'user' in session:
+        return render_template("edit_account.html")
+    abort(401)
 
 # Login and Sign in Page
 @app.get('/login')
